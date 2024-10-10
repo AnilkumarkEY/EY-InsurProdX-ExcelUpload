ed580a33edf655b6f9d3664e6ebfca72998ebb89
JyotiEY <jyoti.sharma18@in.ey.com>
2024-10-10 13:52:23 +0530

    Prod URL changed

--- frontent/src/constants/common.js
+++ frontent/src/constants/common.js
@@ -1,5 +1,5 @@
 const envObject = {
-    VITE_API_BASE_URL_PROD : "http://4.247.28.50/",
+    VITE_API_BASE_URL_PROD : "https://6ac19029-8341-4ffa-9238-a15549e9a09f-dev.e1-us-east-azure.choreoapis.dev/eydemo/excel-upload-framework/v1.0",
     //VITE_API_BASE_URL_PROD : "http://localhost:5000/",
 }
 
